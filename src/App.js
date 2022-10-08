2f90e0412d17b16a71a43f105feb62bf82820e86
SainathKamble <sainath.kamble@vnnogile.in>
2022-10-08 17:56:56 +0530

    todo comoplete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
 import React, {useState, useReducer} from "react";
-function reducer(state, action) {
+import Todo from "./Todo";
+export const ACTIONS =  {
+    ADD_TODO : "add-todo",
+    REMOVE_TODO : "remove-todo",
+    TOGGLE_TODO : "toggle-todo"
+}
+
+
+
+function reducer(todo, action) {
   switch(action.type) {
-    case  "increment" :
-      return {count : state.count + 1}
-    case "decrement" :
-      return {count : state.count - 1}
+    case ACTIONS.ADD_TODO : 
+      return [...todo, newTodo(action.payload.name)]
+
+    case ACTIONS.TOGGLE_TODO : 
+      return todo.map(todo => {
+        if(todo.id === action.payload.id){
+          return {...todo, complete : !todo.complete}
+        }
+        return todo
+      })
+    case ACTIONS.REMOVE_TODO : 
+      return todo.filter(todo => todo.id !== action.payload.id)
+      
   }
 }
 
+function newTodo (name){
+  return {
+    id : Date.now(), name : name , complete : false
+  }
+}
+
+
 function App() {
   
-  const [state, dispatch] = useReducer(reducer, {count : 0})
+  const [todo, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState("")
 
-  function increment() {
-    
-    dispatch({type : "increment"})
-  }
-
-  function decrement (){
-    dispatch({type : "decrement"})
-  }
-
+ function handleSubmit(e) {
+  e.preventDefault()
+  dispatch({type : ACTIONS.ADD_TODO, payload : {name : name}});
+  setName("")
+ }
+ console.log(todo)
   return (
     <>
     
-    <button onClick={decrement}>-</button>
-    <span>{state.count}</span>
-    <button onClick={increment}>+</button>
-    
+   <form onSubmit={handleSubmit}>
+    <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+   </form>
+    {todo.map(todo => {
+      return <Todo key={todo.id} todo = {todo} dispatch = {dispatch}></Todo>
+    })}
     </>
   );
 }
 
-export default App;
+export default App
